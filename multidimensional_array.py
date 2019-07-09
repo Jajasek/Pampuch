7229cb24ed980ee276a7d4421302a645cef1b027
Jáchym <jachym.mierva@gmail.com>
2019-07-09 20:39:36 +0200

    completed multidimensional array

--- multidimensional_array.py
+++ multidimensional_array.py
@@ -1,59 +1,106 @@
+class Enumerating_iterator:
+    def __init__(self, array):
+        self.array = array
+
+    def __iter__(self):
+        self.array.__iter__()
+        return self
+
+    def __next__(self):
+        pos = tuple(self.array._iter_pos)
+        return pos, self.array.__next__()
+
+
 class Multidimensional_array(list):
     def __init__(self, dimensions=(1, 1), iterable=None, fill=None):
+        self.enumerated = Enumerating_iterator(self)
         self._fill = fill
-        self.number_of_dimensions = 0
+        self._number_of_dimensions = 0
+        self._stop = False
         for dim in dimensions:
             if dim <= 0:
                 break
-            self.number_of_dimensions += 1
-        self.dimensions = dimensions[:self.number_of_dimensions]
-        if not self.number_of_dimensions:
+            self._number_of_dimensions += 1
+        self._dimensions = tuple(dimensions[:self._number_of_dimensions])
+        self._iter_pos = None
+        if not self._number_of_dimensions:
             super().__init__()
             return
         try:
             iter(iterable)
         except TypeError:
             iterable = [iterable]
-        if len(iterable) < dimensions[0]:
-            iterable += [fill for _ in range(dimensions[0] - len(iterable))]
-        numdim = self.number_of_dimensions - 1
+        if len(iterable) < dimensions[-1]:
+            iterable += [fill for _ in range(dimensions[-1] - len(iterable))]
+        numdim = self._number_of_dimensions - 1
         if numdim > 0:
-            sublist = [Multidimensional_array(dimensions[1:], item, fill=fill)
-                       for item in iterable[:dimensions[0]]]
+            sublists = [Multidimensional_array(dimensions[:-1], item, fill=fill)
+                        for item in iterable[:dimensions[-1]]]
         else:
-            sublist = iterable[:dimensions[0]]
-        super().__init__(sublist)
+            sublists = iterable[:dimensions[-1]]
+        super().__init__(sublists)
 
-    def __getitem__(self, item):
-        if item is None:
-            item = slice(None)
-        if isinstance(item, int):
-            return list.__getitem__(self, item)
-        if isinstance(item, slice):
-            sliced = list.__getitem__(self, item)
-            return Multidimensional_array((len(sliced),) + self.dimensions[1:], sliced,
-                                          self._fill)
+    def __getitem__(self, coordinates):
         try:
-            coordinate = item[0]
+            iter(coordinates)
         except TypeError:
-            pass
-        else:
-            if coordinate is None:
-                item = (slice(None),) + item[1:]
-            if isinstance(item[0], int):
-                return self[item[0]][item[1:] if len(item) > 2 else item[1]]
-            if isinstance(item[0], slice):
-                sliced = list.__getitem__(self, item[0])
-                sliced = [subarray[item[1:] if len(item) > 2 else item[1]] for subarray in sliced]
-                try:
-                    dim = sliced[0].dimensions
-                except (IndexError, AttributeError):
-                    dim = ()
-                return Multidimensional_array((len(sliced),) + dim, sliced,
-                                              self._fill)
+            coordinates = [coordinates]
+        coordinates = list(coordinates)
+        if len(coordinates) < self._number_of_dimensions:
+            coordinates += [slice(None) for _ in range(self._number_of_dimensions - len(coordinates))]
+        elif len(coordinates) > self._number_of_dimensions:
+            coordinates = coordinates[:self._number_of_dimensions]
+        coordinates.reverse()
+        coordinate = coordinates[0]
+        if coordinate is None:
+            coordinate = slice(None)
+        if isinstance(coordinate, int):
+            subarray = list.__getitem__(self, coordinate)
+            return subarray[coordinates[1:]] if len(coordinates) > 1 else subarray
+        if isinstance(coordinate, slice):
+            sliced = list.__getitem__(self, coordinate)
+            if len(coordinates) > 1:
+                sliced = [subarray[coordinates[1:]] for subarray in sliced]
+            try:
+                dim = sliced[0].get_dimensions()
+            except (IndexError, AttributeError):
+                dim = ()
+            return Multidimensional_array(dim + (len(sliced),), sliced, self._fill)
         raise TypeError('coordinates must be in form of tuple of integers, slices or None')
+
+    def __iter__(self):
+        self._iter_pos = [0] * self._number_of_dimensions
+        self._stop = False
+        return self
+
+    def __next__(self):
+        if self._stop:
+            self._stop = False
+            raise StopIteration
+        item = self[self._iter_pos]
+        for dimension in range(len(self._iter_pos)):
+            self._iter_pos[dimension] += 1
+            if self._iter_pos[dimension] == self.get_dimensions()[dimension]:
+                self._iter_pos[dimension] = 0
+            else:
+                return item
+        self._stop = True
+        return item
+
+    def get_dimensions(self):
+        return self._dimensions
 
 
 if __name__ == "__main__":
-    array = Multidimensional_array((3, 4), ['1234', 'abcd', 'ABCD'])
-    print(array[None, None])
+    array = Multidimensional_array((4, 3), ['1234', 'abcd', 'ABCD'])
+    print(array, end='\n\n')
+    for j in range(array.get_dimensions()[1]):
+        for i in range(array.get_dimensions()[0]):
+            print(array[i, j], end='')
+        print()
+    print()
+    for item in array:
+        print(item)
+    print()
+    for item in array.enumerated:
+        print(item)
